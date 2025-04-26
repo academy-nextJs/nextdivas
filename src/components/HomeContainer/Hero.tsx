9f74d2292828f64a1f90f20ad043530ec96cdc5f
fatemeh_bkhshian <flowerfbf18@gmail.com>
2025-04-26 14:59:20 +0330

    commit

--- src/components/HomeContainer/Hero.tsx
+++ src/components/HomeContainer/Hero.tsx
@@ -1,7 +1,24 @@
+import Image from "next/image";
 import React from "react";
+import hero from "../../../public/landing/hero.png";
 
 const Hero = () => {
-  return <div className="bg-red-500">Hero</div>;
-};
+  return (
+    <div className="relative">
+      <Image src={hero} alt="hero" className="absolute -z-10 w-full h-screen max-md:h-[500px] max-sm:h-[400px] " />
+
+      <div className="border border-red-500">
+        
+        <div className="grid grid-cols-2">
+          <div>left</div>
+          <div>right</div>
+        </div>
+
+        <div>filter</div>
+      </div>
+
+    </div>
+  
+)};
 
 export default Hero;
