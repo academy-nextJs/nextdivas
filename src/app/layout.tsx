7e78d2b09272547a80bf4e54957922ed32391b92
malihe911 <malihe.hashemi2020@gmail.com>
2025-04-27 13:48:36 +0330

    footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>
-          <Header/>
+          <Header />
+          <Footer />
+
           {children}
         </Providers>
       </body>
