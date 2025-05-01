44a0014c3c9a46c5bb1f085e97ba4f9ad7aa7ce9
malihe911 <malihe.hashemi2020@gmail.com>
2025-05-01 21:25:41 +0330

    api first config

---
+++ src/utils/service/auth.ts
@@ -0,0 +1,11 @@
+// src/utils/service/auth.ts
+
+// import axiosInstance from "./axiosInstance";
+
+// export const login = (data: { email: string; password: string }) => {
+//   return axiosInstance.post("/login", data);
+// };
+
+// export const registerStepOne = (data: { email: string }) => {
+//   return axiosInstance.post("/register/step-one", data);
+// };
